cdfadc705297b45bcc0b0573347971ecfb24ac3f
remonc97 <remonc97@gmail.com>
2018-04-28 23:53:48 +0700

    fixing model for every activity

--- application/models/M_activity.php
+++ application/models/M_activity.php
@@ -15,25 +15,60 @@ class M_activity extends CI_Model{
 		return $checkinsert; 		
     }
  
-	function jumlah_data(){
-		return $this->db->get('kegiatan')->num_rows();
+	function jumlah_data($user_id){
+		$result = $this->db->where('user_id',$user_id)->get('ikut_kegiatan');
+		
+		if ($result->num_rows()) {
+			$this->db->select('kegiatan.*, user.full_name');
+			$this->db->where('slot >', '0');
+			$this->db->where('kegiatan_id != (SELECT kegiatan_id FROM ikut_kegiatan WHERE user_id ='.$user_id.')');
+			$this->db->join('user', 'kegiatan.user_id = user.user_id');
+			$this->db->order_by("kegiatan_id", "desc");
+			return $this->db->get('kegiatan')->num_rows();
+		} else {
+			$this->db->select('kegiatan.*, user.full_name');
+			$this->db->where('slot >', '0');
+			$this->db->join('user', 'kegiatan.user_id = user.user_id');
+			$this->db->order_by("kegiatan_id", "desc");
+			return $this->db->get('kegiatan')->num_rows();
+		}
 	}
  
 	function jumlah_data_mine($user_id){
-		$this->db->where('user_id',$user_id);
+		$this->db->select('*');
+		$this->db->where('kegiatan.user_id',$user_id);
+		$this->db->join('user', 'kegiatan.user_id = user.user_id');
+		$this->db->order_by("kegiatan_id", "desc");
 		return $this->db->get('kegiatan')->num_rows();
 	}
  
 	function jumlah_data_join($user_id){
-		$this->db->where('user_id',$user_id);
-		return $this->db->get('ikut_kegiatan')->num_rows();
+		$this->db->select('kegiatan.*, user.full_name, ikut_kegiatan.ikut_id');
+		$this->db->where('ikut_kegiatan.user_id',$user_id);
+		$this->db->join('ikut_kegiatan', 'kegiatan.kegiatan_id = ikut_kegiatan.kegiatan_id');
+		$this->db->join('user', 'kegiatan.user_id = user.user_id');
+		$this->db->order_by("ikut_id", "desc");
+		return $this->db->get('kegiatan')->num_rows();
 	}
 
-    function data($number,$offset){
-		$this->db->select('*');
-		$this->db->join('user', 'kegiatan.user_id = user.user_id');
-		$this->db->order_by("kegiatan_id", "desc");
-		return $query = $this->db->get('kegiatan',$number,$offset)->result();
+    function data($number,$offset,$user_id){
+		$result = $this->db->where('user_id',$user_id)->get('ikut_kegiatan');
+		
+		if ($result->num_rows()) {
+			$this->db->select('kegiatan.*, user.full_name');
+			$this->db->where('slot >', '0');
+			$this->db->where('kegiatan_id != (SELECT kegiatan_id FROM ikut_kegiatan WHERE user_id ='.$user_id.')');
+			$this->db->join('user', 'kegiatan.user_id = user.user_id');
+			$this->db->order_by("kegiatan_id", "desc");
+			return $query = $this->db->get('kegiatan',$number,$offset)->result();
+		} else {
+			$this->db->select('kegiatan.*, user.full_name');
+			$this->db->where('slot >', '0');
+			$this->db->join('user', 'kegiatan.user_id = user.user_id');
+			$this->db->order_by("kegiatan_id", "desc");
+			return $query = $this->db->get('kegiatan',$number,$offset)->result();
+		}
+		
 	}
 
     function data_mine($number,$offset,$user_id){
