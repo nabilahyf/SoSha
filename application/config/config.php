b0208b4cf1515ca235de4fcaf76e972c2678eb89
remonc97 <remonc97@gmail.com>
2018-04-28 20:25:11 +0700

    menyesuaikan untuk base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/SoSha/';
+$config['base_url'] = 'http://206.189.84.220:85/';
 
 /*
 |--------------------------------------------------------------------------
