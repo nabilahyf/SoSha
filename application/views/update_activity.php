5626700b0ea88de8b309b0ac2f3bc0df428a2677
remonc97 <remonc97@gmail.com>
2018-05-13 23:52:30 +0700

    adding google analytic

--- application/views/update_activity.php
+++ application/views/update_activity.php
@@ -21,6 +21,15 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/upload.cs'; ?>s"><!--[if lt IE 9]>        
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 		<![endif]-->
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111542804-2"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-111542804-2');
+		</script>
 	</head>
 	
 	<body>
